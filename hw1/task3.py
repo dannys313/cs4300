73bbf872119236c11696322b72f4bc5ea856f73d
dannys313 <dsteffen313@gmail.com>
2025-02-07 17:23:33 +0000

    alright

--- hw1/task3.py
+++ hw1/task3.py
@@ -14,16 +14,25 @@ def numtest():
         print("number not entered\n")
 
 #function to print out first 10 prime numbers
-def primetest():
-    test = 2
-    num = 0
-    while num < 10:
-        for i in range(2, int(test**0.5) + 1):
-            if test % i != 0:
-                print("{test}\n")
-                num += 1   
-        test += 1
-            
-        
+def prime_test(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5)+1):
+        if n % i == 0:
+            return False
+    return True
 
-primetest()
+def calc_primes():
+    count = 0
+    number = 2
+    prime_numbers = []
+
+    while count < 10:
+        if prime_test(number):
+            prime_numbers.append(number)
+            count += 1
+        number += 1
+            
+    print(prime_numbers)
+
+calc_primes()
